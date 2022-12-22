4db189afd50bf5b6ba8de175601468d95deaa295
和田拓海 <ponzu@wadatakuminoMacBook-Air.local>
2022-12-22 21:36:27 +0900

    15 commit

--- app/controllers/informs_controller.rb
+++ app/controllers/informs_controller.rb
@@ -1,6 +1,6 @@
 class InformsController < ApplicationController
     before_action :authenticate_user
     def index
-     @informs = Inform.where(user: current_user).order(created_at: :DESC)
+     @informs = Inform.where(user_id: current_user.id).order(created_at: :DESC)
     end
 end
